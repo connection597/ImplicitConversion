925350a575f24539a42e12e75b22ede55c9e8958
connection579 <wlswn4118@naver.com>
2020-11-03 18:31:34 +0900

    묵시적 형 변환

---
+++ src/conversion/ImplicitConversion.java
@@ -0,0 +1,13 @@
+package conversion;
+
+public class ImplicitConversion {
+
+	public static void main(String[] args) {
+		byte bNum = 10;
+		int iNum = bNum; //byte 형 값이 int형 변수에 대입됭ㅁ
+		
+		System.out.println(bNum);
+		System.out.println(iNum);
+	}
+
+}
